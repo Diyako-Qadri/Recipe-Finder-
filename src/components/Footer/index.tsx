2d1a203c4ee2fca8b84eca09b5fffad76e7453d2
Diyako-Qadri <qadri.diyako@gmail.com>
2024-10-20 14:01:55 +0200

    maked the link to the page visible if user exist

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,6 +1,10 @@
 import Link from 'next/link';
+import { useUserContext } from '@/utils/contexts';
+import { type userContextType } from '@/utils/types';
 
 const Footer = () => {
+  const { user } = useUserContext() as userContextType;
+
   return (
     <footer className="bg-[#4e9a5d] text-white p-10 text-center  items-center">
       <div className="flex flex-col md:flex-row justify-around border-b-2 border-white">
@@ -13,17 +17,19 @@ const Footer = () => {
             info@recipe.com
           </a>
         </div>
-      
+
         <div className="flex flex-col text-start text-xl  pb-4">
-          <Link href="/">Home</Link>
-          <Link href="/category">Category</Link>
-          <Link href="/profile">Profile</Link>
-          <p className='hover:underline'>Privecy & Policy</p>
+          {user && (
+            <>
+              <Link href="/">Home</Link>
+              <Link href="/category">Category</Link>
+              <Link href="/profile">Profile</Link>
+            </>
+          )}
+          <p className="hover:underline">Privecy & Policy</p>
         </div>
       </div>
-    <div className='p-3'>
-      &copy; All rights reserved by Diyako Qadri
-    </div>
+      <div className="p-3">&copy; All rights reserved by Diyako Qadri</div>
     </footer>
   );
 };
