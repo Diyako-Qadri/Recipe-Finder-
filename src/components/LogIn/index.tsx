45a503fef233d66ae7faa79a75dcae4eeda2cee0
Diyako-Qadri <qadri.diyako@gmail.com>
2024-10-21 17:33:16 +0200

    fixed some typo

--- src/components/LogIn/index.tsx
+++ src/components/LogIn/index.tsx
@@ -22,7 +22,7 @@ const LogIn = () => {
   };
 
   return (
-    <div className="flex fixed  w-[300px] flex-col bg-[#4e9a5d] left-0 right-2 top-[6rem] m-auto pt-6 text-white z-30 abslute justify-center  items-center">
+    <div className="flex fixed  w-[300px] flex-col bg-[#4e9a5d] gap-2 left-0 right-2 top-[6rem] m-auto pt-3 text-white z-30 abslute justify-center  items-center">
       <p>To log in please enter your user name</p>
       <label htmlFor="user-input">Enter User Name</label>
       <input
@@ -31,12 +31,20 @@ const LogIn = () => {
         id="user-input"
         onChange={handleChange}
       />
-      <button
-        className="bg-[#d76721] px-8 py-1 m-3 rounded-[20px]"
-        onClick={handleClick}
-      >
-        Log in
-      </button>
+      <div className='flex flex-col pb-2 gap-0 justify-center items-center'>
+        <button
+          className="bg-[#d76721] px-8 py-1 m-1 w-full  rounded-[20px]"
+          onClick={handleClick}
+        >
+          Log In
+        </button>
+        <button
+          className="bg-[#d76721] px-8 py-1 m-1 w-full rounded-[20px]"
+          onClick={handleClick}
+        >
+          Sign Up
+        </button>
+      </div>
     </div>
   );
 };
