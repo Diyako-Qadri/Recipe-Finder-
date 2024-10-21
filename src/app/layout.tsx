a027205f10f5798ad4f45daa5fd11712c4fa6af2
Diyako-Qadri <qadri.diyako@gmail.com>
2024-10-21 17:03:55 +0200

    fixed some typo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,6 @@ const geistSans = localFont({
   src: './fonts/GeistVF.woff',
   variable: '--font-geist-sans',
   weight: '100 900',
- 
 });
 const geistMono = localFont({
   src: './fonts/GeistMonoVF.woff',
